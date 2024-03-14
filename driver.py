f3643ad93f49e5ea2fcf8d050e68a9d5b5ae09f3
gus-rautenberg <gprautenber@gmail.com>
2024-03-14 17:54:41 -0300

    Acredito que o driver esteja correto

--- driver.py
+++ driver.py
@@ -1,3 +1,35 @@
 # dedutivo
 # Backward
 
+import contextlib
+import sys
+
+from pyke import knowledge_engine
+from pyke import krb_traceback
+
+engine = knowledge_engine.engine(__file__)
+
+    
+def hardware_diagnosis_questions():
+
+    engine.reset()      # Allows us to run tests multiple times.
+
+    engine.activate('rules') #STUDENTS: you will need to edit the name of your rule file here
+
+    print("doing proof")
+    
+    try:
+        with engine.prove_goal('rules.what_to_do($action)') as gen: #STUDENTS: you will need to edit this line
+            for vars, plan in gen:
+                print("Você precisa executar: %s" % (vars['action'])) #STUDENTS: you will need to edit this line
+
+    except Exception:
+        # This converts stack frames of generated python functions back to the
+        # .krb file.
+        krb_traceback.print_exc()
+        sys.exit(1)
+
+    print()
+    print("done")
+
+
